3d3c1d59453bce9285b095152a9dceca2a300609
jin.liu@soyatec.com <jin.liu@soyatec.com>
2020-02-07 11:17:37 +0800

    Remove boot starter for explorer

--- fabric-explorer-spring-boot-starter/src/main/java/io/github/ecsoya/fabric/explorer/FabricExplorerApplication.java
+++ fabric-explorer-spring-boot-starter/src/main/java/io/github/ecsoya/fabric/explorer/FabricExplorerApplication.java
@@ -1,22 +1,16 @@
 package io.github.ecsoya.fabric.explorer;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication
-@ComponentScan(basePackages = "io.github.ecsoya.fabric.explorer.*")
-public class FabricExplorerApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FabricExplorerApplication.class);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(FabricExplorerApplication.class, args);
-	}
-
-}
+//@SpringBootApplication
+//@ComponentScan(basePackages = "io.github.ecsoya.fabric.explorer.*")
+//public class FabricExplorerApplication extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(FabricExplorerApplication.class);
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(FabricExplorerApplication.class, args);
+//	}
+//
+//}
